84b9a556eb1b53b22ae955e52a8c6096832537ef
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2024-11-08 21:18:27 +0100

    Add arrays_hashCode_a and arrays_equal

---
+++ converted/arrays_hashCode_a.py
@@ -0,0 +1,17 @@
+from nagini_contracts.contracts import *
+
+def hashCode_a(a: list[int]) -> int:
+    Requires(Acc(list_pred(a)))
+
+    result = 1
+    ic = 0
+
+    while ic < len(a):
+        Invariant(Acc(list_pred(a)))
+        Invariant(len(a) == Old(len(a)))
+        Invariant(0 <= ic and ic <= len(a))
+        element = a[ic]
+        result = 31 * result + element
+        ic += 1
+
+    return result
