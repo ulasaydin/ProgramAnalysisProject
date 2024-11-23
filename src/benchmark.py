27671c3155a7d5f0b744eccfdc5022cb695570db
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2024-11-23 21:39:40 +0000

    Add output directory command line arguments to benchmark script

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,16 +1,32 @@
 from datetime import datetime
 import os
+from tkinter import FALSE
 from platformdirs import user_data_dir
 import sys
-sys.path.append(os.path.dirname(__file__))
+import argparse
+
+current_dir = os.path.dirname(__file__)
+
+sys.path.append(current_dir)
 
 from config import APP_AUTHOR, APP_NAME, TEST_CASES
 from invariant_finder import find_invariants
 
-user_data_directory = user_data_dir(APP_NAME, APP_AUTHOR)
+parser = argparse.ArgumentParser(prog="Loop Invariant Finder Benchmark")
+parser.add_argument(
+    "-o",
+    "--output-dir",
+    help="Output directory",
+    required=False,
+)
+args = parser.parse_args()
 
-benchmark_dir_name = f"Benchmark-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-benchmark_dir = os.path.join(user_data_directory, benchmark_dir_name)
+if args.output_dir:
+    benchmark_dir = args.output_dir
+else:
+    user_data_directory = user_data_dir(APP_NAME, APP_AUTHOR)
+    benchmark_dir_name = f"Benchmark-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    benchmark_dir = os.path.join(user_data_directory, benchmark_dir_name)
 
 print(f"Running benchmark and writing results to {benchmark_dir}:")
 
@@ -19,4 +35,8 @@ for program_file_path, entry_point_method in TEST_CASES:
     output_dir_name = f"{os.path.basename(program_file_path)}-{entry_point_method}"
     output_dir = os.path.join(benchmark_dir, output_dir_name)
 
-    find_invariants(os.path.normpath(os.path.join(os.path.dirname(__file__), program_file_path)), entry_point_method, output_dir)
+    find_invariants(
+        os.path.normpath(os.path.join(os.path.dirname(__file__), program_file_path)),
+        entry_point_method,
+        output_dir,
+    )
